36d7600b5c018aa551a81376edf12bddef924887
Leonard Wang <leonardwang2000@gmail.com>
2019-02-13 16:49:00 -0800

    Initial Commit

---
+++ CS32Project3WarmUp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS32Project3WarmUp
+//
+//  Created by Leonard Wang on 2/13/19.
+//  Copyright © 2019 Leonard Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
